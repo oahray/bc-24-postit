44547bdf921c2a7482de8b0d3fcf4f0d2450cd3b
Oahray <rodneyoare@gmail.com>
2017-07-13 14:32:02 +0100

    Modify package.json with postinstall script"

--- server/app.js
+++ server/app.js
@@ -39,8 +39,7 @@ app.use((req, res, next) => {
   if (req.cookies.user_sid && !req.session.user) {
     res.clearCookie('user_sid');
   }
-  next();
-});
+  next();5});
 
 // Require our routes
 require('./routes')(app);
